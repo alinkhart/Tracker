1e8231d82e8af7bebe9cab7453599c8c110dde43
Adam Linkhart <alinkhart@gmail.com>
2018-04-01 21:04:35 -0400

    App Center configuration

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+    
+        var appCenterSecret = Bundle.main.infoDictionary?["APP_CENTER_SECRET"] as? String
+        
         return true
     }
 
