095112f22191492f96a37c584ec9da7a5fedf9b8
Adam Linkhart <alinkhart@gmail.com>
2018-05-19 18:15:10 -0400

    All navigation is now handled in the AppCoordinator

--- Tracker/Launch/AppCoordinator.swift
+++ Tracker/Launch/AppCoordinator.swift
@@ -9,23 +9,33 @@
 import UIKit
 
 class AppCoordinator: WelcomeViewControllerDelegate, LoginViewControllerDelegate, CreateAccountViewControllerDelegate,
-CreatePasscodeViewControllerDelegate, PasscodeViewControllerDelegate {
+    CreatePasscodeViewControllerDelegate, PasscodeViewControllerDelegate, TracksTableViewControllerDelegate {
     
     private let welcomeViewController = WelcomeViewController.makeFromStoryboard()
     private let createAccountViewController = CreateAccountViewController.makeFromStoryboard()
     private let loginViewController = LoginViewController.makeFromStoryboard()
     private let createPasscodeViewController = CreatePasscodeViewController.makeFromStoryboard()
     private let passcodeViewController = PasscodeViewController.makeFromStoryboard()
+    private let createTrackViewController = CreateTrackViewController.makeFromStoryboard()
+    private let trackDetailsViewController = TrackDetailsViewController.makeFromStoryboard()
+    
     private let rootTabBarController: UITabBarController
+    private let tracksTableViewController: TracksTableViewController
+    private let settingsViewController: SettingsViewController
     
     init() {
         self.rootTabBarController = UIStoryboard(name: "Tabs", bundle: nil).instantiateViewController(withIdentifier: "RootTabBarController") as! UITabBarController
+        let tracksNavController = self.rootTabBarController.viewControllers![0] as! UINavigationController
+        self.tracksTableViewController = tracksNavController.viewControllers[0] as! TracksTableViewController
+        let settingsNavController = self.rootTabBarController.viewControllers![1] as! UINavigationController
+        self.settingsViewController = settingsNavController.viewControllers[0] as! SettingsViewController
         
-        welcomeViewController.delegate = self
-        loginViewController.delegate = self
-        createAccountViewController.delegate = self
-        createPasscodeViewController.delegate = self
-        passcodeViewController.delegate = self
+        self.welcomeViewController.delegate = self
+        self.loginViewController.delegate = self
+        self.createAccountViewController.delegate = self
+        self.createPasscodeViewController.delegate = self
+        self.passcodeViewController.delegate = self
+        self.tracksTableViewController.delegate = self
     }
     
     func start() -> UIViewController {
@@ -62,5 +72,13 @@ CreatePasscodeViewControllerDelegate, PasscodeViewControllerDelegate {
     
     func didSelectEnterPasscode() {
         passcodeViewController.present(rootTabBarController, animated: true, completion: nil)
+    }
+    
+    func didSelectCreateTrack() {
+        tracksTableViewController.present(createTrackViewController, animated: true, completion: nil)
+    }
+    
+    func didSelectTrack() {
+        tracksTableViewController.navigationController?.pushViewController(trackDetailsViewController, animated: true)
     }
 }
--- Tracker/Tracks/TracksTableViewController.swift
+++ Tracker/Tracks/TracksTableViewController.swift
@@ -8,24 +8,21 @@
 
 import UIKit
 
+protocol TracksTableViewControllerDelegate: class {
+    func didSelectCreateTrack()
+    func didSelectTrack()
+}
+
 final class TracksTableViewController: UITableViewController, TracksStoryboardInitializable {
+    
+    weak var delegate: TracksTableViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func CreateTrackButtonTapped(_ sender: Any) {
+        delegate?.didSelectCreateTrack()
     }
     
     // MARK: - Table view data source
@@ -43,50 +40,8 @@ final class TracksTableViewController: UITableViewController, TracksStoryboardIn
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.didSelectTrack()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
