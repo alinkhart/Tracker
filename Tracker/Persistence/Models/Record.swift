e233d890f8464ff539b49e78022902aca0bae3de
Adam Linkhart <alinkhart@gmail.com>
2018-05-20 17:15:55 -0400

    Created Record model class

---
+++ Tracker/Persistence/Models/Record.swift
@@ -0,0 +1,17 @@
+//
+//  Record.swift
+//  Tracker
+//
+//  Created by alinkhart on 5/20/18.
+//  Copyright © 2018 Adam Linkhart. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Record: Object {
+    @objc dynamic var entry = ""
+    @objc dynamic var createdDate = Date(timeIntervalSinceNow: 0)
+    @objc dynamic var deletedDate = Date(timeIntervalSinceNow: 0)
+    @objc dynamic var lastModifiedDate = Date(timeIntervalSinceNow: 0)
+}
