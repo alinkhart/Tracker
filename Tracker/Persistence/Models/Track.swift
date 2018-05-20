8cc1c22d24a59af7a50048ae15b2c9d335d8ead7
Adam Linkhart <alinkhart@gmail.com>
2018-05-20 17:09:27 -0400

    Created Track model class

---
+++ Tracker/Persistence/Models/Track.swift
@@ -0,0 +1,17 @@
+//
+//  Track.swift
+//  Tracker
+//
+//  Created by alinkhart on 5/20/18.
+//  Copyright © 2018 Adam Linkhart. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Track: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var createdDate = Date(timeIntervalSinceNow: 0)
+    @objc dynamic var lastModifiedDate: Date? = nil
+    @objc dynamic var deletedDate: Date? = nil
+}
