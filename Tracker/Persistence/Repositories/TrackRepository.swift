53881d0e26688c167cd596d0f135267a1cdc1678
Adam Linkhart <alinkhart@gmail.com>
2018-05-21 04:27:36 -0400

    Created TrackRepository class

---
+++ Tracker/Persistence/Repositories/TrackRepository.swift
@@ -0,0 +1,14 @@
+//
+//  TrackRepository.swift
+//  Tracker
+//
+//  Created by alinkhart on 5/20/18.
+//  Copyright © 2018 Adam Linkhart. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class TrackRepository {
+    
+}
